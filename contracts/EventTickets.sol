370fe20294c854f3a325b16d6d6fb05a366f5206
eguyonnet <32397301+eguyonnet@users.noreply.github.com>
2019-06-01 18:19:26 +0200

    Update

--- contracts/EventTickets.sol
+++ contracts/EventTickets.sol
@@ -11,6 +11,7 @@ contract EventTickets {
         Use the appropriate keyword to create an associated getter function.
         Use the appropriate keyword to allow ether transfers.
      */
+    address payable public owner;
 
     uint   TICKET_PRICE = 100 wei;
 
@@ -20,8 +21,18 @@ contract EventTickets {
         Choose the appropriate variable type for each field.
         The "buyers" field should keep track of addresses and how many tickets each buyer purchases.
     */
-
+    struct Event {
+        string description;
+        string website;
+        uint totalTickets;
+        uint sales;
+        mapping (address => uint) buyers;
+        bool isOpen;
+    }
+    
     Event myEvent;
+
+    //uint ticketsCountForSale;
 
     /*
         Define 3 logging events.
@@ -29,10 +40,17 @@ contract EventTickets {
         LogGetRefund should provide information about the refund requester and the number of tickets refunded.
         LogEndSale should provide infromation about the contract owner and the balance transferred to them.
     */
+    event LogBuyTickets(address indexed _buyer, uint _ticketsCount);
+    event LogGetRefund(address indexed _buyer, uint _ticketsCount);
+    event LogEndSale(address indexed _owner, uint _totalAmount);
 
     /*
         Create a modifier that throws an error if the msg.sender is not the owner.
     */
+    modifier onlyOwner() { 
+        require(msg.sender == owner, "Not authorized"); 
+        _; 
+    }    
      
     /*
         Define a constructor.
@@ -40,18 +58,22 @@ contract EventTickets {
         Set the owner to the creator of the contract.
         Set the appropriate myEvent details.
     */
-    
+    constructor(string memory _description, string memory _website, uint _totalTicketsForSale) public {
+        owner = msg.sender;
+        myEvent.description = _description;
+        myEvent.website = _website;
+        myEvent.totalTickets = _totalTicketsForSale;
+        myEvent.sales = 0;
+        myEvent.isOpen = true;
+    }
+
     /*
         Define a funciton called readEvent() that returns the event details.
         This function does not modify state, add the appropriate keyword.
         The returned details should be called description, website, uint totalTickets, uint sales, bool isOpen in that order.
     */
-    function readEvent() 
-
-        public 
-        returns(string memory description, string memory website, uint totalTickets, uint sales, bool isOpen) 
-    {
-
+    function readEvent() public view returns (string memory description, string memory website, uint totalTickets, uint sales, bool isOpen) {
+        return (myEvent.description, myEvent.website, myEvent.totalTickets, myEvent.sales, myEvent.isOpen);
     }
 
     /*
@@ -59,7 +81,12 @@ contract EventTickets {
         This function takes 1 argument, an address and
         returns the number of tickets that address has purchased.
     */
-     
+    function getBuyerTicketCount(address _buyer) public view returns (uint) {
+        require(_buyer != address(0), "Invalid buyer address");
+        return myEvent.buyers[_buyer];
+    }
+
+
     /*
         Define a function called buyTickets().
         This function allows someone to purchase tickets for the event.
@@ -75,7 +102,24 @@ contract EventTickets {
             - refund any surplus value sent with the transaction
             - emit the appropriate event
     */
-    
+    function buyTickets(uint _ticketsCount) public payable {
+        require(myEvent.isOpen == true, "Event is closed");
+        require(_ticketsCount <= (myEvent.totalTickets-myEvent.sales), "Not enought tickets for sale");
+        uint _totalPrice = mul(_ticketsCount, TICKET_PRICE);
+        require(msg.value >= _totalPrice, "No bagain");
+
+        myEvent.buyers[msg.sender] += _ticketsCount;
+        myEvent.sales += _ticketsCount;
+        // Refund if has paid more than expected
+        uint _amountToRefund = msg.value - _totalPrice;
+        if (_amountToRefund > 0) {
+          msg.sender.transfer(_amountToRefund);
+        }
+
+        emit LogBuyTickets(msg.sender, _ticketsCount);
+    }
+
+
     /*
         Define a function called getRefund().
         This function allows someone to get a refund for tickets for the account they purchased from.
@@ -85,7 +129,20 @@ contract EventTickets {
             - Transfer the appropriate amount to the refund requester.
             - Emit the appropriate event.
     */
-    
+    function getRefund() public payable {
+        require(myEvent.isOpen, "Event is closed");
+        require(myEvent.buyers[msg.sender] > 0, "No tickets found");
+        
+        uint _ticketsRefunded = myEvent.buyers[msg.sender];
+        uint _amountRefunded = mul(_ticketsRefunded, TICKET_PRICE);
+        msg.sender.transfer(_amountRefunded);
+        myEvent.buyers[msg.sender] = 0;
+        myEvent.sales -= _ticketsRefunded;
+        
+        emit LogGetRefund(msg.sender, _ticketsRefunded);
+    }
+
+
     /*
         Define a function called endSale().
         This function will close the ticket sales.
@@ -95,4 +152,32 @@ contract EventTickets {
             - transfer the contract balance to the owner
             - emit the appropriate event
     */
+    function endSale() public onlyOwner {
+        require(myEvent.isOpen == true, "Event is already closed");
+        myEvent.isOpen = false;
+        uint _total = mul(TICKET_PRICE, myEvent.sales);
+        if (_total > 0) {
+            owner.transfer(_total);
+        }
+
+        emit LogEndSale(owner, _total);
+    }
+
+    /**
+     * @dev Multiplies two unsigned integers, reverts on overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b);
+
+        return c;
+    }
+
 }
